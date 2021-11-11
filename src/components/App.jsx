3038a89fe58d3a7b276eb7d14cccfffb21e59d8d
Devorris Russell <devorris_r@yahoo.com>
2021-11-11 11:49:50 -0500

    Added new folder for apps and made title bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,42 @@
 import React, {Component} from 'react';
+import './App.css'
+import TitleBar from './TitleBar/TitleBar';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.books = [
+            {title: "Ready Player One", author: "Ernest Cline"},
+            {title: "All the Light We Cannot See", author: "Anthony Doerr"},
+            {title: "The First and Last Freedom", author: "Jiddu Krishnamurit"}
+
+        ]; 
+        this.state = {
+            bookNumber: 0
+        };
     }
+
     render(){
         return (
-          <h1> Our React App from A Component File </h1>  
+          <div clasName="container-fluid">
+              <TitleBar />
+              <div className="row">
+              <div className='col-md-4'>
+                  {/*Button here to move to the previous book viewed*/}
+              </div>
+              
+              <div className='col-md-4'>
+                  {/*Display book with cover here*/}
+                  <h1>{this.books[this.state.bookNumber].title} </h1>
+                  <h4>{this.books[0].author}</h4>
+
+              </div>
+
+              <div className='col-md-4'>
+                  {/*Button here to move to the next book viewed*/}
+              </div>
+          </div>
+          </div>
         )
     }
 }
